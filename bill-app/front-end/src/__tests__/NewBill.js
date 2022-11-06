20f87c4fbc7c768ebb253b7d1b919a4e16e51c63
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-11-06 12:05:45 +0100

    ESLint_tests

--- bill-app/front-end/src/__tests__/NewBill.js
+++ bill-app/front-end/src/__tests__/NewBill.js
@@ -1,18 +1,23 @@
+/* eslint-disable no-undef */
 /**
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+//  ESLint :'describe' is not defined
+//  ESLint :'test' is not defined
+//  ESLint :'NewBill' is defined but never used !!!
+//  ESLint :'screen' is defined but never used !!!
 
+import { screen } from '@testing-library/dom'
+import NewBillUI from '../views/NewBillUI.js'
+import NewBill from '../containers/NewBill.js'
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+describe('Given I am connected as an employee', () => {
+  describe('When I am on NewBill Page', () => {
+    test('Then ...', () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      // to-do write assertion
     })
   })
 })
--- bill-app/front-end/src/__tests__/VerticalLayout.js
+++ bill-app/front-end/src/__tests__/VerticalLayout.js
@@ -1,17 +1,21 @@
+/* eslint-disable no-undef */
 /**
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import VerticalLayout from "../views/VerticalLayout"
-import { localStorageMock } from "../__mocks__/localStorage.js"
+//  ESLint :'describe' is not defined
+//  ESLint :'test' is not defined
+//  ESLint :'expect' is not defined
 
+import { screen } from '@testing-library/dom'
+import VerticalLayout from '../views/VerticalLayout'
+import { localStorageMock } from '../__mocks__/localStorage.js'
 
 describe('Given I am connected as Employee', () => {
-  test("Then Icons should be rendered", () => {
+  test('Then Icons should be rendered', () => {
     Object.defineProperty(window, 'localStorage', { value: localStorageMock })
     const user = JSON.stringify({
-      type: 'Employee'
+      type: 'Employee',
     })
     window.localStorage.setItem('user', user)
     const html = VerticalLayout(120)
@@ -19,5 +23,4 @@ describe('Given I am connected as Employee', () => {
     expect(screen.getByTestId('icon-window')).toBeTruthy()
     expect(screen.getByTestId('icon-mail')).toBeTruthy()
   })
-
 })
